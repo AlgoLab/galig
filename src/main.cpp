ea9f7ad0ca2551e4aea6b0c0c3d350f1ffcad859
Luca Denti <l.denti@campus.unimib.it>
2017-06-14 15:13:53 +0200

    Cleans main

--- src/main.cpp
+++ src/main.cpp
@@ -100,35 +100,41 @@ int main(int argc, char* argv[]) {
     outFile.open(out);
     while(i<fastas.getSize()) {
         std::pair<std::string, std::string> seq = fastas.getEntry(i);
+
         std::string read = seq.second;
+        std::pair<int, std::list<std::list<Mem> > > paths;
         std::list<Mem> mems = bm.getMEMs(read,L);
-
-        MemsGraph mg (sg, read, mems, L, eps, verbose);
-        mg.build(sg, mems);
-        std::pair<int, std::list<std::list<Mem> > > paths = mg.visit();
-
-        std::string read1 = reverse_and_complement(read);
-        std::list<Mem> mems1 = bm.getMEMs(read1,L);
-        MemsGraph mg1 (sg, read1, mems, L, eps, verbose);
-        mg1.build(sg, mems1);
-        std::pair<int, std::list<std::list<Mem> > > paths1 = mg1.visit();
-        
-        int flag = 0;
-        if(paths.first <= paths1.first) {
-            if(paths.second.size() != 0) {
-                flag = 1;
-            } else if(paths1.second.size() != 0) {
-                flag = 2;
-            }
-        } else {
-            if(paths1.second.size() != 0) {
-                flag = 2;
-            } else if(paths1.second.size() != 0) {
-                flag = 1;
-            }
+        bool flag = false;
+        if(mems.size() != 0) {
+            MemsGraph mg (sg, read, L, eps, verbose);
+            mg.build(sg, mems);
+            paths = mg.visit();
+            flag = true;
         }
 
-        switch(flag) {
+        std::string read_RC = reverse_and_complement(read);
+        std::pair<int, std::list<std::list<Mem> > > paths_RC;
+        std::list<Mem> mems_RC = bm.getMEMs(read_RC,L);
+        bool flag_RC = false;
+        if(mems_RC.size() != 0) {
+            MemsGraph mg_RC (sg, read_RC, L, eps, verbose);
+            mg_RC.build(sg, mems_RC);
+            paths_RC = mg_RC.visit();
+            flag_RC = true;
+        }
+        int best = 0;
+        if(flag && !flag_RC) {
+            best = 1;
+        } else if(!flag && flag_RC) {
+            best = 2;
+        } else {
+            if(paths.first <= paths_RC.first) {
+                best = 1;
+            } else {
+                best = 2;
+            }
+        }
+        switch(best) {
         case 0:
             break;
         case 1:
@@ -141,8 +147,8 @@ int main(int argc, char* argv[]) {
             }
             break;
         case 2:
-            for(std::list<std::list<Mem> >::iterator p=paths1.second.begin(); p!=paths1.second.end(); ++p) {
-                outFile << "- " << seq.first << " " << paths1.first << " ";
+            for(std::list<std::list<Mem> >::iterator p=paths_RC.second.begin(); p!=paths_RC.second.end(); ++p) {
+                outFile << "- " << seq.first << " " << paths_RC.first << " ";
                 for(std::list<Mem>::iterator m=p->begin(); m!=p->end(); ++m) {
                     outFile << m->toStr() << " ";
                 }
