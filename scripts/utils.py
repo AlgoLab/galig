388075bc92c0bfc0ec3e7e341a570a120cd9b6b0
Luca Denti <l.denti@openmailbox.org>
2017-06-28 22:51:29 +0200

    Adds utils and Common Ancestors/Descendants

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,140 +1,72 @@
-import gffutils
-from Bio import SeqIO
+import itertools
 
-class Annotation:
-    def __init__(self, genomic_path, gtf_path):
-        genomic = SeqIO.index(genomic_path, "fasta")
-        try:
-            gtf = gffutils.FeatureDB("{}.db".format(gtf_path),
-                                     keep_order=True)
-        except ValueError:
-            gtf = gffutils.create_db(gtf_path,
-                                     dbfn="{}.db".format(gtf_path),
-                                     force=True, keep_order=True,
-                                     disable_infer_genes=True,
-                                     disable_infer_transcripts=True,
-                                     merge_strategy='merge',
-                                     sort_attribute_values=True)
-        self.T = "|"
-        edges = []
-        added_exons = {}
-        ex_index = 1
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
-        self.gene = ""
-        self.transcripts = {}
-        self.exons = {}
-        self.introns = {}
-        self.exons_name = [""]
-        self.exons_length = [-1]
-        self.strand = True
-        for gene in gtf.features_of_type('gene'):
-            chromo = gene.seqid
-            sequence = genomic[chromo]
-            self.strand = gene.strand == '+'
-            self.gene = gene.id
-            for tr in gtf.children(gene, featuretype='transcript', order_by='start'):
-                t_id = tr.id
-                self.transcripts.update({t_id : tr.start})
-                prev_start = -1
-                Es = []
-                Is = []
-                last_id = -1
-                for ex in gtf.children(tr, featuretype='exon', order_by='start'):
-                    start = ex.start
-                    end = ex.end
-                    ex_posid = str(start) + "_" + str(end)
-                    Es.append([start, end])
-                    if prev_start != -1:
-                        Is.append([prev_start, start-1])
-                    prev_start = end+1
-                    if ex_posid not in added_exons:
-                        ex_seq = sequence[ex.start-1:ex.end-1+1].seq
-                        self.T += ex_seq + "|"
-                        self.exons_length.append(len(ex_seq))
-                        self.exons_name.append(ex.attributes['exon_id'][0])
-                        added_exons.update({ex_posid:ex_index})
-                        ex_index+=1
-                    if last_id != -1:
-                        edges.append((last_id, ex_posid))
-                    last_id = ex_posid
-                self.exons.update({t_id : Es})
-                self.introns.update({t_id : Is})
+#################################
+### ANCESTORS AND DESCENDANTS ###
+#################################
 
-        self.transcripts_length = {}
-        for tr_id,Es in self.exons.items():
-            l = 0
-            for (s,e) in Es:
-                l+=e-s
-            self.transcripts_length.update({tr_id:l})
+#Utils
+def bitOr(lists):
+    res = lists[0]
+    for l in lists[1:]:
+        res = [i | j for i,j in zip(res,l)]
+    return res
 
-        self.BV = BitVector(self.T)
+def bitAnd(list1, list2):
+    return [i & j for i,j in zip(list1, list2)]
 
-        self.adj_matrix = []
-        for i in range(0, ex_index+1):
-            row = []
-            for j in range(0, ex_index+1):
-                row.append(0)
-            self.adj_matrix.append(row)
-        for edge in edges:
-            self.adj_matrix[added_exons[edge[0]]][added_exons[edge[1]]] = 1
+def getColumn(A, c):
+    return [row[c] for row in A]
 
-    def getExonLength(self, i):
-        return self.exons_length[i]
+#Ancestors
+def getAncestors(A, x):
+    ancestors = []
+    used = []
+    toCheck = [getColumn(A,x)]
+    used.append(x)
+    while len(toCheck)>0:
+        curr_c = toCheck.pop(0)
+        ancestors.append(curr_c)
+        r = 0
+        for elem in curr_c:
+            if elem == 1 and r not in used:
+                used.append(r)
+                next_c = getColumn(A,r)
+                if sum(next_c) > 0:
+                    toCheck.append(next_c)
+            r+=1
+    return bitOr(ancestors)
 
-    def getExonName(self, i):
-        return self.exons_name[i]
-    
-    def contain(self, i, j):
-        return self.adj_matrix[i][j]
+def getCommonAncestors(A, x, y):
+    anc_x = getAncestors(A, x)
+    anc_y = getAncestors(A, y)
+    return bitAnd(anc_x, anc_y)
 
-    def rank(self, i):
-        return self.BV.rank(i)
+#Descendants
+def getDescendants(A, x):
+    descendants = []
+    used = []
+    toCheck = [A[x]]
+    used.append(x)
+    while len(toCheck)>0:
+        curr_r = toCheck.pop(0)
+        descendants.append(curr_r)
+        c = 0
+        for elem in curr_r:
+            if elem == 1 and c not in used:
+                used.append(c)
+                next_r = A[c]
+                if sum(next_r) > 0:
+                    toCheck.append(next_r)
+            c+=1
+    return bitOr(descendants)
 
-    def select(self, i):
-        return self.BV.select(i)
-    
-class BitVector:
-    '''
-    Il bit vector indicizza da 0. 
-    Presenta un 1 dove nel testo di trova un "|"
-    '''
-    def __init__(self, text):
-        self.text = text
-        
-        self.length = len(text)
-        
-        self.bit_vector = []
-        self.ones = []
-        i = 1
-        for c in self.text:
-            if c == "|":
-                self.bit_vector.append(1)
-                self.ones.append(i)
-            else:
-                self.bit_vector.append(0)
-            i += 1
-
-    def rank(self, i):
-        n = 0
-        for elem in self.ones:
-            if elem<=int(i):
-                n+=1
-            else:
-                break
-        return n
-    
-    def select(self, i):
-        iterator = 0
-        for elem in self.ones:
-            iterator+=1
-            if iterator == i:
-                return elem
-
-    def __str__(self):
-        s = ""
-        for elem in self.bit_vector:
-            s += str(elem)
-        return s
-
-if __name__ == '__main__':
-    pass
+def getCommonDescendants(A, x, y):
+    anc_x = getDescendants(A, x)
+    anc_y = getDescendants(A, y)
+    return bitAnd(anc_x, anc_y)
