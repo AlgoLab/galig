AlgoLab/galig

commit ca4153cd69536406e554cbf79cd4f29a66b4cd62

Text index is used for MEMs computation, if already created

src/bMEM.cpp
@@ -2,15 +2,15 @@
 
 BackwardMEM::BackwardMEM(const std::string& text, const std::string& path) {
     std::string index_path = path + ".idx";
-    sdsl::construct_im(cst, text, 1);
-    // if(!load_from_file(cst, index_path)) {
-    //     std::cout << "Creating index..." << std::endl;
-    //     sdsl::construct_im(cst, text, 1);
-    //     sdsl::store_to_file(cst, index_path);
-    //     std::cout << "Saving index..." << std::endl;
-    // } else {
-    //     std::cout << "Loading index from disk..." << std::endl;
-    // }
+    //sdsl::construct_im(cst, text, 1);
+    if(!load_from_file(cst, index_path)) {
+        //std::cout << "Creating index..." << std::endl;
+        sdsl::construct_im(cst, text, 1);
+        sdsl::store_to_file(cst, index_path);
+        //std::cout << "Saving index..." << std::endl;
+    } //else {
+        //std::cout << "Loading index from disk..." << std::endl;
+    //}
 }
 
 tCST::size_type BackwardMEM::backward_search(const tCST::csa_type& csa,

commit 594487e8a587f96af79a8e3e9b7d686943e4eaa6

Index for MEMs computation is always build (bug introduced by ca4153c)

src/bMEM.cpp
@@ -2,15 +2,7 @@
 
 BackwardMEM::BackwardMEM(const std::string& text, const std::string& path) {
     std::string index_path = path + ".idx";
-    //sdsl::construct_im(cst, text, 1);
-    if(!load_from_file(cst, index_path)) {
-        //std::cout << "Creating index..." << std::endl;
-        sdsl::construct_im(cst, text, 1);
-        sdsl::store_to_file(cst, index_path);
-        //std::cout << "Saving index..." << std::endl;
-    } //else {
-        //std::cout << "Loading index from disk..." << std::endl;
-    //}
+    sdsl::construct_im(cst, text, 1);
 }
 
 tCST::size_type BackwardMEM::backward_search(const tCST::csa_type& csa,