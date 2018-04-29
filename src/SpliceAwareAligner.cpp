AlgoLab/galig

commit c94072edcab718e1261f2b9862263a5df09cd8c3

Major update
- adds native support to fasta and fastq (also gzipped)
- cleans splicing graph construction from GTF

src/SpliceAwareAligner.cpp
@@ -5,16 +5,21 @@
 #include <utility>
 #include <list>
 
+#include <zlib.h>  
+#include <stdio.h>  
+
+#include "kseq.h"
+
 #include "SplicingGraph.hpp"
 #include "bMEM.hpp"
 #include "utils.hpp"
-
 #include "MEMsGraph.hpp"
+
+KSEQ_INIT(gzFile, gzread)
 
 void printHelp() {
     std::cout << "Usage: SGAL [options] (required: -g -a -s -o)\n" << std::endl;
     std::cout << "Options:" << std::endl;
-    //std::cout << "  -I, --index: index only" << std::endl;
     std::cout << "  -g, --genome <path>" << std::endl;
     std::cout << "  -a, --annotation <path>" << std::endl;
     std::cout << "  -s, --sample <path>" << std::endl;
@@ -89,7 +94,6 @@ int main(int argc, char* argv[]) {
     int L = 0;
     int eps = -1;
     std::string out;
-    bool index_only = false;
     bool verbose = false;
 
     // - Collecting command line parameters
@@ -98,7 +102,6 @@ int main(int argc, char* argv[]) {
     while (1) {
         static struct option long_options[] =
             {
-                //{"index", no_argument, 0, 'I'},
                 {"genomic", required_argument, 0, 'g'},
                 {"annotation", required_argument, 0, 'a'},
                 {"sample",  required_argument, 0, 's'},
@@ -118,9 +121,6 @@ int main(int argc, char* argv[]) {
         }
 
         switch(c) {
-        // case 'I':
-        //     index_only = true;
-        //     break;
         case 'g':
             genomic = optarg;
             break;
@@ -151,7 +151,7 @@ int main(int argc, char* argv[]) {
         }
     }
     if(L == 0) {
-        L=15;
+        L = 15;
     }
     if(eps < 0 || eps > 100) {
         eps = 3;
@@ -159,62 +159,40 @@ int main(int argc, char* argv[]) {
 
     // - Building splicing graph
     // --------------------------
-    SplicingGraph sg (genomic, annotation);
-    if(verbose) sg.print();
-    if(index_only) return 0;
+    gzFile fastain = gzopen(genomic.c_str(), "r");
+    kseq_t *reference = kseq_init(fastain);
+    kseq_read(reference);
+    
+    SplicingGraph sg (reference->seq.s, annotation);
+
+    // sg.print();
+    kseq_destroy(reference);
+    gzclose(fastain);
+
     int exsN = sg.getExonsNumber();
 
     // - Setting up MEMs Index and out file
     // ---------------------------------------------------
     BackwardMEM bm (sg.getText(), genomic);
+
     std::ofstream outFile;
     outFile.open(out);
 
-    std::ifstream fastaFile (rna_seqs);
+    fastain = gzopen(rna_seqs.c_str(), "r");
     std::pair<char, std::list<std::pair<int, std::list<Mem> > > > paths;
+    
+    kseq_t *seqs = kseq_init(fastain);  
+    int l;
+    std::string head;
+    std::string read;
+    int i = 1;
+    
     // - Main loop: one iteration, one read
     // ---------------------------------------
-    int i = 1;
-    if(fastaFile.is_open()) {
-        std::string line;
-        std::string head;
-        std::string read;
-        while(getline(fastaFile,line)) {
-            if(line.compare("") != 0) {
-                if(line[0] == '>') {
-                    if(read.compare("") != 0) {
-                        paths = analyzeRead(bm, sg, read, L, eps, exsN, verbose);
-                        if(paths.first != '/') {
-                            for(std::pair<int, std::list<Mem> > path : paths.second) {
-                                if(!path.second.empty()) {
-                                    int err = path.first;
-                                    outFile << paths.first << " " << head << " " << err << " ";
-                                    for(std::list<Mem>::iterator m=path.second.begin(); m!=path.second.end(); ++m) {
-                                        outFile << m->toStr() << " ";
-                                    }
-                                    if(paths.first == '+')
-                                        outFile << read;
-                                    else
-                                        outFile << reverseAndComplement(read);
-                                    outFile << "\n";
-                                }
-                            }
-                        }
-                        read = "";
-                        if(i%10000 == 0) {
-                            std::cerr << i << " ";
-                        }
-                        if(i%100000 == 0) {
-                            std::cerr << std::endl;
-                        }
-                        ++i;
-                    }
-                    head = line.substr(1,line.size()-1);
-                } else {
-                    read += line;
-                }
-            }
-        }
+    while ((l = kseq_read(seqs)) >= 0) {
+        head = seqs->name.s;
+        read = seqs->seq.s;
+
         paths = analyzeRead(bm, sg, read, L, eps, exsN, verbose);
         if(paths.first != '/') {
             for(std::pair<int, std::list<Mem> > path : paths.second) {
@@ -232,9 +210,11 @@ int main(int argc, char* argv[]) {
                 }
             }
         }
-    } else {
-        std::cerr << "Reads file not found!" << std::endl;
+        if(i%10000 == 0)
+            std::cout << "Processed " << i << " genes." << std::endl;
+        ++i;
     }
-    outFile.close();
-    std::cout << std::endl;
+    kseq_destroy(seqs);
+    gzclose(fastain);
+    return 0;
 }

src/SplicingGraph.cpp
@@ -4,127 +4,110 @@ std::string getExonID(int s, int e) {
     return std::to_string(s) + ":" + std::to_string(e);
 }
 
-SplicingGraph::SplicingGraph(const std::string& fa,
-                             const std::string& gtf) {
-    std::ifstream fastaFile (fa);
-    std::string line;
-    std::string head;
-    std::string genomic;
-    if(fastaFile.is_open()) {
-        while(getline(fastaFile,line)) {
-            if(line.compare("") != 0) {
-                if(line[0] == '>') {
-                    head = line.substr(1,line.size()-1);
-                } else {
-                    genomic += line;
-                }
-            }
-        }
-    } else {
-        std::cerr << "Genomic file not found!" << std::endl;
-    }
-    refLen = genomic.size();
+SplicingGraph::SplicingGraph(const char *genomic,
+                             const std::string &gtf) {
+    refLen = strlen(genomic);
 
     std::ifstream gtfFile;
-    std::map<std::string, std::list<std::string> > genes;
-    std::map<std::string, std::list<std::string> > transcripts;
-    std::map<std::string, Feature> exons;
-    std::map<std::string, std::string> addedExons;
-    std::string currGene;
-    std::string currTr;
-    exsN = 0;
 
-    gtfFile.open(gtf);
-    if(gtfFile.is_open()) {
-        while(getline(gtfFile,line)) {
-            Feature f (line);
-            if(f.type.compare("gene") == 0) {
-                reference = f.seqid;
-                currGene = f.id;
-                strand = f.strand;
-                genes.insert(std::make_pair(currGene, std::list<std::string> ()));
-                addedExons.clear();
-            }
-            else if(f.type.compare("transcript") == 0 || f.type.compare("mRNA") == 0) {
-                currTr = f.id;
-                genes.at(currGene).push_back(currTr);
-                transcripts.insert(std::pair<std::string, std::list<std::string> >(currTr, std::list<std::string> ()));
-            }
-            else if(f.type.compare("exon") == 0) {
-                std::string ex = f.id;
-                transcripts.at(currTr).push_back(ex);
-                try {
-                    addedExons.at(ex);
-                } catch(const std::out_of_range& oor) {
-                    addedExons[ex] = "";
-                    exons.insert(std::make_pair(ex, f));
-                    ++exsN;
-                }
-            }
-        }
-    }
-
-    addedExons.clear();
-    //Questo prima exsN è una stima superiore del vero numero di esoni,
-    //dato che lo stesso esone può essere messo in due trascritti con ID diverso.
-    //Nella passata successiva, mi baso sulla combinazione start_end
-    edges.resize(exsN+1);
-    parents.resize(exsN+1);
-    sons.resize(exsN+1);
-    for(int i = 0; i <= exsN; i++) {
-        edges[i] = std::vector<int>(exsN+1, 0);
-    }
+    edges.push_back({0});
+    edges.push_back({0});
+    
     T = "|";
 
     int exID = 1;
     std::map<std::string, int> id2index;
     int curr_i = 1;
-    std::unordered_map<std::string, int> realEdges;
-    for(std::map<std::string, std::list<std::string> >::iterator it1=genes.begin(); it1!=genes.end(); ++it1) {
-        //Forall gene
-        for(std::list<std::string>::iterator it2=it1->second.begin(); it2!=it1->second.end(); ++it2) {
-            //Forall transcript in gene
-            std::list<int> exonsID;
-            int last_i = -1;
-            for(std::list<std::string>::iterator it3=transcripts[*it2].begin(); it3!=transcripts[*it2].end(); ++it3) {
-                //Forall exon in transcript
-                std::string exonID = *it3;
-                Feature e = exons[exonID];
-                std::string posID = getExonID(e.start, e.end);
-                try {
-                    addedExons.at(posID);
-                    exonsID.push_back(id2index[posID]);
-                } catch(const std::out_of_range& oor) {
-                    addedExons.insert(std::make_pair(posID, ""));
+    int last_i = -1;
+    
+    std::string line;
+    gtfFile.open(gtf);
+    if(gtfFile.is_open()) {
+        while(getline(gtfFile,line)) {
+            Feature feat (line);
+            if(feat.type.compare("gene") == 0) {
+                reference = feat.seqid;
+                strand = feat.strand;
+            }
+            else if(feat.type.compare("transcript") == 0 || feat.type.compare("mRNA") == 0) {
+                last_i = -1;
+            }
+            else if(feat.type.compare("exon") == 0) {
+                std::string posID = getExonID(feat.start, feat.end);
+
+                if (id2index.find(posID) == id2index.end()) {
                     id2index[posID] = exID;
-                    exonsID.push_back(exID);
-                    std::string currExString = genomic.substr(e.start-1, e.end-e.start+1);
+                    std::string currExString(genomic + feat.start-1, feat.end-feat.start+1);
                     T += currExString + "|";
-                    ExonsPos.push_back(std::make_pair(e.start, e.end));
-                    ExonsName.push_back(e.id);
+                    ExonsPos.push_back(std::make_pair(feat.start, feat.end));
                     ++exID;
                 }
+
                 curr_i = id2index[posID];
                 if(last_i != -1) {
-                    if(last_i != curr_i && edges[last_i][curr_i] == 0) {
-                        realEdges[std::to_string(ExonsPos[last_i-1].second) + std::to_string(ExonsPos[curr_i-1].first)] = 1;
+                    if(last_i != curr_i) {
+                        if(last_i >= (int)edges.size()) {
+                            int i = edges.size();
+                            while(i<=last_i+1) {
+                                edges.push_back({0});
+                                ++i;
+                            }
+                        }
+
+                        if(curr_i >= (int)edges[last_i].size()) {
+                            int i = edges[last_i].size();
+                            while(i<=curr_i+1) {
+                                edges[last_i].push_back(0);
+                                ++i;
+                            }
+                        }
+                        //edges[last_i] vector<vector<int>> rightValidVariants(1, {i});
                         edges[last_i][curr_i] = 1;
+                        if(curr_i >= (int)parents.size()) {
+                            int i = 0;
+                            while(i<=curr_i+1) {
+                                parents.push_back({});
+                                ++i;
+                            }
+                        }
                         parents[curr_i].push_back(last_i);
+
+                        if(last_i >= (int)sons.size()) {
+                            int i = 0;
+                            while(i<=last_i+1) {
+                                sons.push_back({});
+                                ++i;
+                            }
+                        }
                         sons[last_i].push_back(curr_i);
                     }
                 }
                 last_i = curr_i;
             }
-            exonsID.clear();
         }
     }
 
-    //Transitive closure on the graph
+    // Transitive closure on the graph
     int i = 1;
     for(const std::pair<int,int>& p1 : ExonsPos) {
+        if(i>=(int)edges.size()) {
+            int i_ = edges.size();
+            while(i_<=i+1) {
+                edges.push_back({0});
+                ++i_;
+            }
+        }
         int j = 1;
         for(const std::pair<int,int>& p2 : ExonsPos) {
             if(p1.second <= p2.first) {
+                if(j>=(int)edges[i].size()) {
+                    int j_ = edges[i].size();
+                    while(j_<=j+1) {
+                        edges[i].push_back(0);
+                        ++j_;
+                    }
+                }
+
                 if(edges[i][j] == 0) {
                     edges[i][j] = 2;
                     parents[j].push_back(i);
@@ -135,57 +118,7 @@ SplicingGraph::SplicingGraph(const std::string& fa,
         }
         ++i;
     }
-    /**
-    int i = 1;
-    for(const std::pair<int,int>& p1 : ExonsPos) {
-        int start1 = p1.first;
-        int end1 = p1.second;
-        int j = 1;
-        for(const std::pair<int,int>& p2 : ExonsPos) {
-            int start2 = p2.first;
-            int end2 = p2.second;
-            if(end1 <= start2) {
-                if(edges[i][j] == 0) {
-                    //If the intron end/start of e1/e2 is already an edge (different start/end), it is not a new edge
-                    bool isAnnIntron = false; //realEdges.find(std::to_string(end1) + std::to_string(start2)) != realEdges.end();
-                    //Here we check for alternative end/start of e1/e2 (but equal start/end)
-                    if(!isAnnIntron) {
-                        std::list<int> FirstExons;
-                        std::list<int> SecondExons;
-                        for(const std::pair<int,int>& p3 : ExonsPos) {
-                            int start3 = p3.first;
-                            int end3 = p3.second;
-                            if(start1 == start3 or end1 == end3) {
-                                FirstExons.push_back(end3);
-                            } else if(start2 == start3 or end2 == end3) {
-                                SecondExons.push_back(start3);
-                            }
-                        }
-                        for(const int& e : FirstExons) {
-                            for(const int& s : SecondExons) {
-                                if(realEdges.find(std::to_string(e) + std::to_string(s)) != realEdges.end()) {
-                                    isAnnIntron = true;
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    if(isAnnIntron) {
-                        edges[i][j] = 1;
-                    } else {
-                        edges[i][j] = 2;
-                    }
-                    parents[j].push_back(i);
-                    sons[i].push_back(j);
-                }
-            }
-            ++j;
-        }
-        ++i;
-    }
-    **/
     gtfFile.close();
-
     setupBitVector();
     save(gtf);
 }
@@ -301,9 +234,9 @@ void SplicingGraph::save(const std::string path) {
         ofile << p.first << "," << p.second << " ";
     }
     ofile << "\n";
-    for(const std::string& name : ExonsName) {
-        ofile << name << " ";
-    }
+    // for(const std::string& name : ExonsName) {
+    //     ofile << name << " ";
+    // }
     ofile << "\n";
     ofile.close();
 }

Makefile
@@ -56,7 +56,7 @@ OBJS_SpliceAwareAligner = \
 	SpliceAwareAligner.o
 
 #LIBS_main= $(LIBS) $(LOC_DIR)/include/snap-core/Snap.o -lrt -lsdsl -ldivsufsort -ldivsufsort64 -lemon
-LIBS_SpliceAwareAligner= $(LIBS) -lrt -lsdsl -ldivsufsort -ldivsufsort64 -lemon
+LIBS_SpliceAwareAligner= $(LIBS) -lrt -lsdsl -ldivsufsort -ldivsufsort64 -lemon -lz
 
 #
 # END List of programs

asgal
@@ -43,7 +43,7 @@ print_help ()
     echo "Required parameters:"
     echo -e "  -g, --genome GENOME\t\t\treference in FASTA format"
     echo -e "  -a, --annotation ANNOTATION\t\tanotation in GTF format"
-    echo -e "  -s, --sample SAMPLE\t\t\tRNA-Seq sample in FASTA/Q format"
+    echo -e "  -s, --sample SAMPLE\t\t\tRNA-Seq sample in FASTA/Q format (gzip allowed)"
     echo -e "  -o, --output OUTPUT\t\t\tOutput file (without extension)"
     echo ""
     echo "Optional parameters:"
@@ -189,17 +189,6 @@ then
     echo "### Empty output file"
     echo ""
     exit 1
-fi
-
-# Convert fastq to fasta
-sampleExt="${sample##*.}"
-if [ "${sampleExt}" = "fastq" ] || [ "${sampleExt}" = "fq" ]
-then
-    echo "* (0/3) Converting FASTQ sample to FASTA format"
-    bn=$(basename $sample ".${sampleExt}")
-    python3 ${WD}/scripts/fastq2fasta.py ${sample} $bn".fasta"
-    sample=$bn".fasta"
-    echo ""
 fi
 
 # Creating folder for output

scripts/fastq2fasta.py
@@ -1,19 +1,0 @@
-import sys, os
-
-from Bio import SeqIO
-
-def main():
-    fastqPath = sys.argv[1]
-    fastaPath = sys.argv[2]
-
-    if not os.path.exists(fastqPath):
-        print("No FASTQ file at", fastqPath)
-        sys.exit(1)
-    
-    with open(fastqPath, "rU") as fastq:
-        with open(fastaPath, "w") as fasta:
-            sequences = SeqIO.parse(fastq, "fastq")
-            SeqIO.write(sequences, fasta, "fasta")
-
-if __name__ == '__main__':
-    main()

src/SplicingGraph.hpp
@@ -125,7 +125,6 @@ private:
     std::vector<std::list<int> > parents;
     std::vector<std::list<int> > sons;
     int exsN;
-    std::list<std::string> ExonsName;
 
     std::vector<std::vector<int> > edges;
     sdsl::rrr_vector<> bitVector;
@@ -137,7 +136,7 @@ private:
     //void load(const std::string);
 public:
     //SplicingGraph(const std::string&);
-    SplicingGraph(const std::string&, const std::string&);
+    SplicingGraph(const char*, const std::string&);
     std::string getText() const;
     std::list<int> getParents(const int&) const;
     std::list<int> getSons(const int&) const;

src/kseq.h
@@ -1,0 +1,242 @@
+/* The MIT License
+
+   Copyright (c) 2008, 2009, 2011 Attractive Chaos <attractor@live.co.uk>
+
+   Permission is hereby granted, free of charge, to any person obtaining
+   a copy of this software and associated documentation files (the
+   "Software"), to deal in the Software without restriction, including
+   without limitation the rights to use, copy, modify, merge, publish,
+   distribute, sublicense, and/or sell copies of the Software, and to
+   permit persons to whom the Software is furnished to do so, subject to
+   the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
+/* Last Modified: 05MAR2012 */
+
+#ifndef AC_KSEQ_H
+#define AC_KSEQ_H
+
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define KS_SEP_SPACE 0 // isspace(): \t, \n, \v, \f, \r
+#define KS_SEP_TAB   1 // isspace() && !' '
+#define KS_SEP_LINE  2 // line separator: "\n" (Unix) or "\r\n" (Windows)
+#define KS_SEP_MAX   2
+
+#define __KS_TYPE(type_t)						\
+	typedef struct __kstream_t {				\
+		unsigned char *buf;						\
+		int begin, end, is_eof;					\
+		type_t f;								\
+	} kstream_t;
+
+#define ks_err(ks) ((ks)->end == -1)
+#define ks_eof(ks) ((ks)->is_eof && (ks)->begin >= (ks)->end)
+#define ks_rewind(ks) ((ks)->is_eof = (ks)->begin = (ks)->end = 0)
+
+#define __KS_BASIC(type_t, __bufsize)								\
+	static inline kstream_t *ks_init(type_t f)						\
+	{																\
+		kstream_t *ks = (kstream_t*)calloc(1, sizeof(kstream_t));	\
+		ks->f = f;													\
+		ks->buf = (unsigned char*)malloc(__bufsize);				\
+		return ks;													\
+	}																\
+	static inline void ks_destroy(kstream_t *ks)					\
+	{																\
+		if (ks) {													\
+			free(ks->buf);											\
+			free(ks);												\
+		}															\
+	}
+
+#define __KS_GETC(__read, __bufsize)						\
+	static inline int ks_getc(kstream_t *ks)				\
+	{														\
+		if (ks_err(ks)) return -3;							\
+		if (ks->is_eof && ks->begin >= ks->end) return -1;	\
+		if (ks->begin >= ks->end) {							\
+			ks->begin = 0;									\
+			ks->end = __read(ks->f, ks->buf, __bufsize);	\
+			if (ks->end == 0) { ks->is_eof = 1; return -1;}	\
+			if (ks->end == -1) { ks->is_eof = 1; return -3;}\
+		}													\
+		return (int)ks->buf[ks->begin++];					\
+	}
+
+#ifndef KSTRING_T
+#define KSTRING_T kstring_t
+typedef struct __kstring_t {
+	size_t l, m;
+	char *s;
+} kstring_t;
+#endif
+
+#ifndef kroundup32
+#define kroundup32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
+#endif
+
+#define __KS_GETUNTIL(__read, __bufsize)								\
+	static int ks_getuntil2(kstream_t *ks, int delimiter, kstring_t *str, int *dret, int append) \
+	{																	\
+		int gotany = 0;													\
+		if (dret) *dret = 0;											\
+		str->l = append? str->l : 0;									\
+		for (;;) {														\
+			int i;														\
+			if (ks_err(ks)) return -3;									\
+			if (ks->begin >= ks->end) {									\
+				if (!ks->is_eof) {										\
+					ks->begin = 0;										\
+					ks->end = __read(ks->f, ks->buf, __bufsize);		\
+					if (ks->end == 0) { ks->is_eof = 1; break; }		\
+					if (ks->end == -1) { ks->is_eof = 1; return -3; }	\
+				} else break;											\
+			}															\
+			if (delimiter == KS_SEP_LINE) { \
+				for (i = ks->begin; i < ks->end; ++i) \
+					if (ks->buf[i] == '\n') break; \
+			} else if (delimiter > KS_SEP_MAX) {						\
+				for (i = ks->begin; i < ks->end; ++i)					\
+					if (ks->buf[i] == delimiter) break;					\
+			} else if (delimiter == KS_SEP_SPACE) {						\
+				for (i = ks->begin; i < ks->end; ++i)					\
+					if (isspace(ks->buf[i])) break;						\
+			} else if (delimiter == KS_SEP_TAB) {						\
+				for (i = ks->begin; i < ks->end; ++i)					\
+					if (isspace(ks->buf[i]) && ks->buf[i] != ' ') break; \
+			} else i = 0; /* never come to here! */						\
+			if (str->m - str->l < (size_t)(i - ks->begin + 1)) {		\
+				str->m = str->l + (i - ks->begin) + 1;					\
+				kroundup32(str->m);										\
+				str->s = (char*)realloc(str->s, str->m);				\
+			}															\
+			gotany = 1;													\
+			memcpy(str->s + str->l, ks->buf + ks->begin, i - ks->begin); \
+			str->l = str->l + (i - ks->begin);							\
+			ks->begin = i + 1;											\
+			if (i < ks->end) {											\
+				if (dret) *dret = ks->buf[i];							\
+				break;													\
+			}															\
+		}																\
+		if (!gotany && ks_eof(ks)) return -1;							\
+		if (str->s == 0) {												\
+			str->m = 1;													\
+			str->s = (char*)calloc(1, 1);								\
+		} else if (delimiter == KS_SEP_LINE && str->l > 1 && str->s[str->l-1] == '\r') --str->l; \
+		str->s[str->l] = '\0';											\
+		return str->l;													\
+	} \
+	static inline int ks_getuntil(kstream_t *ks, int delimiter, kstring_t *str, int *dret) \
+	{ return ks_getuntil2(ks, delimiter, str, dret, 0); }
+
+#define KSTREAM_INIT(type_t, __read, __bufsize) \
+	__KS_TYPE(type_t)							\
+	__KS_BASIC(type_t, __bufsize)				\
+	__KS_GETC(__read, __bufsize)				\
+	__KS_GETUNTIL(__read, __bufsize)
+
+#define kseq_rewind(ks) ((ks)->last_char = (ks)->f->is_eof = (ks)->f->begin = (ks)->f->end = 0)
+
+#define __KSEQ_BASIC(SCOPE, type_t)										\
+	SCOPE kseq_t *kseq_init(type_t fd)									\
+	{																	\
+		kseq_t *s = (kseq_t*)calloc(1, sizeof(kseq_t));					\
+		s->f = ks_init(fd);												\
+		return s;														\
+	}																	\
+	SCOPE void kseq_destroy(kseq_t *ks)									\
+	{																	\
+		if (!ks) return;												\
+		free(ks->name.s); free(ks->comment.s); free(ks->seq.s);	free(ks->qual.s); \
+		ks_destroy(ks->f);												\
+		free(ks);														\
+	}
+
+/* Return value:
+   >=0  length of the sequence (normal)
+   -1   end-of-file
+   -2   truncated quality string
+   -3   error reading stream
+ */
+#define __KSEQ_READ(SCOPE) \
+	SCOPE int kseq_read(kseq_t *seq) \
+	{ \
+		int c,r; \
+		kstream_t *ks = seq->f; \
+		if (seq->last_char == 0) { /* then jump to the next header line */ \
+			while ((c = ks_getc(ks)) >= 0 && c != '>' && c != '@'); \
+			if (c < 0) return c; /* end of file or error*/ \
+			seq->last_char = c; \
+		} /* else: the first header char has been read in the previous call */ \
+		seq->comment.l = seq->seq.l = seq->qual.l = 0; /* reset all members */ \
+		if ((r=ks_getuntil(ks, 0, &seq->name, &c)) < 0) return r;  /* normal exit: EOF or error */ \
+		if (c != '\n') ks_getuntil(ks, KS_SEP_LINE, &seq->comment, 0); /* read FASTA/Q comment */ \
+		if (seq->seq.s == 0) { /* we can do this in the loop below, but that is slower */ \
+			seq->seq.m = 256; \
+			seq->seq.s = (char*)malloc(seq->seq.m); \
+		} \
+		while ((c = ks_getc(ks)) >= 0 && c != '>' && c != '+' && c != '@') { \
+			if (c == '\n') continue; /* skip empty lines */ \
+			seq->seq.s[seq->seq.l++] = c; /* this is safe: we always have enough space for 1 char */ \
+			ks_getuntil2(ks, KS_SEP_LINE, &seq->seq, 0, 1); /* read the rest of the line */ \
+		} \
+		if (c == '>' || c == '@') seq->last_char = c; /* the first header char has been read */	\
+		if (seq->seq.l + 1 >= seq->seq.m) { /* seq->seq.s[seq->seq.l] below may be out of boundary */ \
+			seq->seq.m = seq->seq.l + 2; \
+			kroundup32(seq->seq.m); /* rounded to the next closest 2^k */ \
+			seq->seq.s = (char*)realloc(seq->seq.s, seq->seq.m); \
+		} \
+		seq->seq.s[seq->seq.l] = 0;	/* null terminated string */ \
+		if (c != '+') return seq->seq.l; /* FASTA */ \
+		if (seq->qual.m < seq->seq.m) {	/* allocate memory for qual in case insufficient */ \
+			seq->qual.m = seq->seq.m; \
+			seq->qual.s = (char*)realloc(seq->qual.s, seq->qual.m); \
+		} \
+		while ((c = ks_getc(ks)) >= 0 && c != '\n'); /* skip the rest of '+' line */ \
+		if (c == -1) return -2; /* error: no quality string */ \
+		while ((c = ks_getuntil2(ks, KS_SEP_LINE, &seq->qual, 0, 1) >= 0 && seq->qual.l < seq->seq.l)); \
+		if (c == -3) return -3; /* stream error */ \
+		seq->last_char = 0;	/* we have not come to the next header line */ \
+		if (seq->seq.l != seq->qual.l) return -2; /* error: qual string is of a different length */ \
+		return seq->seq.l; \
+	}
+
+#define __KSEQ_TYPE(type_t)						\
+	typedef struct {							\
+		kstring_t name, comment, seq, qual;		\
+		int last_char;							\
+		kstream_t *f;							\
+	} kseq_t;
+
+#define KSEQ_INIT2(SCOPE, type_t, __read)		\
+	KSTREAM_INIT(type_t, __read, 16384)			\
+	__KSEQ_TYPE(type_t)							\
+	__KSEQ_BASIC(SCOPE, type_t)					\
+	__KSEQ_READ(SCOPE)
+
+#define KSEQ_INIT(type_t, __read) KSEQ_INIT2(static, type_t, __read)
+
+#define KSEQ_DECLARE(type_t) \
+	__KS_TYPE(type_t) \
+	__KSEQ_TYPE(type_t) \
+	extern kseq_t *kseq_init(type_t fd); \
+	void kseq_destroy(kseq_t *ks); \
+	int kseq_read(kseq_t *seq);
+
+#endif

commit b31ef3554fecdfadca6393cfa545a15f563a62f1

Solves a bug in the splicing graph construction
Introduced by commit c94072edcab718e1261f2b9862263a5df09cd8c3

src/SpliceAwareAligner.cpp
@@ -5,8 +5,8 @@
 #include <utility>
 #include <list>
 
-#include <zlib.h>  
-#include <stdio.h>  
+#include <zlib.h>
+#include <stdio.h>
 
 #include "kseq.h"
 
@@ -162,7 +162,7 @@ int main(int argc, char* argv[]) {
     gzFile fastain = gzopen(genomic.c_str(), "r");
     kseq_t *reference = kseq_init(fastain);
     kseq_read(reference);
-    
+
     SplicingGraph sg (reference->seq.s, annotation);
 
     // sg.print();
@@ -180,13 +180,13 @@ int main(int argc, char* argv[]) {
 
     fastain = gzopen(rna_seqs.c_str(), "r");
     std::pair<char, std::list<std::pair<int, std::list<Mem> > > > paths;
-    
-    kseq_t *seqs = kseq_init(fastain);  
+
+    kseq_t *seqs = kseq_init(fastain);
     int l;
     std::string head;
     std::string read;
     int i = 1;
-    
+
     // - Main loop: one iteration, one read
     // ---------------------------------------
     while ((l = kseq_read(seqs)) >= 0) {

src/SplicingGraph.cpp
@@ -12,6 +12,9 @@ SplicingGraph::SplicingGraph(const char *genomic,
 
     edges.push_back({0});
     edges.push_back({0});
+
+    parents.push_back({});
+    sons.push_back({});
     
     T = "|";
 
@@ -40,6 +43,8 @@ SplicingGraph::SplicingGraph(const char *genomic,
                     std::string currExString(genomic + feat.start-1, feat.end-feat.start+1);
                     T += currExString + "|";
                     ExonsPos.push_back(std::make_pair(feat.start, feat.end));
+                    parents.push_back({});
+                    sons.push_back({});
                     ++exID;
                 }
 