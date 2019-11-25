41ad3f41b5b1d20181f9f6173b959bbcc9bad569
Luca Denti <ldenti92@gmail.com>
2019-11-25 14:24:55 +0100

    Fixes asgal-docker.sh script

--- asgal-docker.sh
+++ asgal-docker.sh
@@ -4,21 +4,24 @@ if [ -s /data/transcripts.fa ]
 then
     if [ -s /data/sample_2.fa ]
     then
-	./asgal --multi \
-		-g /data/genome.fa \
-		-a /data/annotation.gtf \
-		-s /data/sample_1.fa \
-		-s2 /data/sample_2.fa \
-		-t /data/transcripts.fa \
-		-o /data/output
+	/galig/asgal --multi \
+		     -g /data/genome.fa \
+		     -a /data/annotation.gtf \
+		     -s /data/sample_1.fa \
+		     -s2 /data/sample_2.fa \
+		     -t /data/transcripts.fa \
+		     -o /data/output
     else
-	./asgal --multi \
-		-g /data/genome.fasta \
-		-a /data/annotation.gtf \
-		-s /data/sample_1.fa \
-		-t /data/transcripts.fa \
-		-o /data/output
+	/galig/asgal --multi \
+		     -g /data/genome.fasta \
+		     -a /data/annotation.gtf \
+		     -s /data/sample_1.fa \
+		     -t /data/transcripts.fa \
+		     -o /data/output
     fi
 else
-    ./asgal -g /data/genome.fasta -a /data/annotation.gtf -s /data/sample_1.fa -o /data/output
+    /galig/asgal -g /data/genome.fasta \
+		 -a /data/annotation.gtf \
+		 -s /data/sample_1.fa \
+		 -o /data/output
 fi
