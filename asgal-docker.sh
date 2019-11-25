fb055cc1806b3f5a70475aa53e4894d413180e13
Luca Denti <ldenti92@gmail.com>
2019-11-25 13:55:37 +0100

    Updates asgal-docker.sh script

--- asgal-docker.sh
+++ asgal-docker.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-if [-s /data/transcripts.txt]
+if [ -s /data/transcripts.fa ]
 then
-  if [-s /data/sample2.fa]
-  then
-    ./asgal --multi \
-        -g /data/genome.fasta \
-        -a [annotation.gtf] \
-        -s [sample1.fa] \
-        -s2 [sample2.fa] \
-        -t [transcripts.fasta] \
-        -o /data
-   else
-     ./asgal --multi \
-        -g /data/genome.fasta \
-        -a [annotation.gtf] \
-        -s [sample1.fa] \
-        -t [transcripts.fasta] \
-        -o /data
-   fi
+    if [ -s /data/sample_2.fa ]
+    then
+	./asgal --multi \
+		-g /data/genome.fa \
+		-a /data/annotation.gtf \
+		-s /data/sample_1.fa \
+		-s2 /data/sample_2.fa \
+		-t /data/transcripts.fa \
+		-o /data/output
+    else
+	./asgal --multi \
+		-g /data/genome.fasta \
+		-a /data/annotation.gtf \
+		-s /data/sample_1.fa \
+		-t /data/transcripts.fa \
+		-o /data/output
+    fi
 else
-  ./asgal -g /data/genome.fasta -a [annotation] -s [sample] -o /data
+    ./asgal -g /data/genome.fasta -a /data/annotation.gtf -s /data/sample_1.fa -o /data/output
 fi
