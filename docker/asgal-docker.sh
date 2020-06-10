d50b84549b798d6791232bd111e1e4069da41cc5
Gianluca Della Vedova <gianluca@dellavedova.org>
2020-06-10 14:51:01 +0200

    Update dockerfile
    And make it easier to discover

--- docker/asgal-docker.sh
+++ docker/asgal-docker.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# Add local user
+# with the same owner as /data
+USER_ID=$(stat -c %u /data)
+GROUP_ID=$(stat -c %g /data)
+
+echo "Starting with UID:GID $USER_ID:$GROUP_ID"
+# We create user:group with the correct uid:gid
+groupadd -g "$GROUP_ID" group
+useradd --shell /bin/bash -u "$USER_ID" -g group -o -c "" -m user
+export HOME=/
+chown --recursive "$USER_ID":"$GROUP_ID" /data
+
 if [ -s /data/transcripts.fa ]
 then
     if [ -s /data/sample_2.fa ]
     then
+    gosu user:group \
 	/galig/asgal --multi \
 		     -g /data/genome.fa \
 		     -a /data/annotation.gtf \
@@ -12,6 +25,7 @@ then
 		     -t /data/transcripts.fa \
 		     -o /data/output
     else
+    gosu user:group \
 	/galig/asgal --multi \
 		     -g /data/genome.fa \
 		     -a /data/annotation.gtf \
@@ -20,6 +34,7 @@ then
 		     -o /data/output
     fi
 else
+    gosu user:group \
     /galig/asgal -g /data/genome.fa \
 		 -a /data/annotation.gtf \
 		 -s /data/sample_1.fa \
